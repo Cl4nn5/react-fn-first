d707ae23847ca894c0980b24d4e87ad4c329c7bc
Clann_Home:김홍제_집 <restoreclann@gmail.com>
2022-09-15 09:37:50 +0900

    React Create

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 
 function Header(props) {
@@ -30,7 +31,7 @@ function Nav(props) {
           href={`/read/${t.id}`}
           onClick={(event) => {
             event.preventDefault();
-            props.onChangeMode(event.target.id);
+            props.onChangeMode(+event.target.id);
           }}
         >
           {t.title}
@@ -56,28 +57,99 @@ function Article(props) {
   );
 }
 
+function Create(props) {
+  return (
+    <article>
+      <h2>Create</h2>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          const title = event.target.title.value;
+          const body = event.target.body.value;
+          props.onCreate(title, body);
+        }}
+      >
+        <p>
+          <input type="text" name="title" placeholder="Title" />
+        </p>
+        <p>
+          <textarea name="body" placeholder="Body"></textarea>
+        </p>
+        <p>
+          <input type="submit" value="Create" />
+        </p>
+      </form>
+    </article>
+  );
+}
+
 function App() {
-  const topics = [
+  const [mode, setMode] = useState("WELCOME");
+  const [id, setId] = useState(null);
+  const [topics, setTopics] = useState([
     { id: 1, title: "html", body: "html is ..." },
     { id: 2, title: "css", body: "css is ..." },
     { id: 3, title: "javascript", body: "js is ..." },
-  ];
+  ]);
+  const [nextId, setNextId] = useState(topics.length + 1);
+  let content = null;
+
+  // mode에 따라서 content를 다르게 보여준다.
+  if (mode === "WELCOME") {
+    content = <Article title="Welcome" body="Hello, WEB" />;
+  } else if (mode === "READ") {
+    let title,
+      body = null;
+
+    topics
+      .filter((t) => t.id === id)
+      .map((t) => {
+        title = t.title;
+        body = t.body;
+      });
+    content = <Article title={title} body={body} />;
+  } else if (mode === "CREATE") {
+    content = (
+      <Create
+        onCreate={(_title, _body) => {
+          // topics에 추가
+          const newTopic = { id: nextId, title: _title, body: _body };
+          const newTopics = [...topics, newTopic];
+          setTopics(newTopics); // topics 업데이트
+          setMode("READ"); // READ로 변경
+          setId(nextId); // id 변경
+          setNextId(nextId + 1); // nextId 변경
+        }}
+      ></Create>
+    );
+  }
 
   return (
     <div>
       <Header
         title="WEB"
         onChangeMode={() => {
-          alert("hi");
+          setMode("WELCOME");
         }}
       ></Header>
       <Nav
         topics={topics}
         onChangeMode={(id) => {
-          alert(id);
+          setMode("READ");
+          setId(id);
         }}
       ></Nav>
-      <Article title="Welcome" body="Hello, WEB"></Article>
+      {/* <Article title="Welcome" body="Hello, WEB"></Article> */}
+      {content}
+      <a
+        href="/create"
+        onClick={(event) => {
+          event.preventDefault();
+          setMode("CREATE");
+        }}
+      >
+        Create Mode
+      </a>
     </div>
   );
 }
